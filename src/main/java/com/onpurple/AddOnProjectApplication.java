d93ee5aa44e0caae14f152d39cfd253e74cee88c
lesu <46406965+leeesu@users.noreply.github.com>
2023-08-25 20:36:09 +0900

    Initial commit

---
+++ src/main/java/com/onpurple/AddOnProjectApplication.java
@@ -0,0 +1,13 @@
+package com.onpurple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddOnProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddOnProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onpurple/AddOnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onpurple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddOnProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
