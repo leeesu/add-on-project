23667d7c6128dee8bed90d8f5d72daa18805543e
lesu <46406965+leeesu@users.noreply.github.com>
2023-09-02 04:35:50 +0900

    [Refact] create, update, detail builder 분리 및 중복코드 제거(#5)

--- src/main/java/com/onpurple/dto/request/PostRequestDto.java
+++ src/main/java/com/onpurple/dto/request/PostRequestDto.java
@@ -1,17 +1,25 @@
 package com.onpurple.dto.request;
 
+import com.onpurple.dto.response.PostResponseDto;
+import com.onpurple.model.Post;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostRequestDto {
   private String title;
   private String content;
-  private String imageUrl;
+  private List<String> imgList;
   private String category;
   private String createdAt;
   private String modifiedAt;
+
+
 }
