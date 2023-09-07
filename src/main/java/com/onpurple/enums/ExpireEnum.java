04a7d93f060d3110bc9df3ba312d158962836dba
lesu <46406965+leeesu@users.noreply.github.com>
2023-09-08 04:55:11 +0900

    [Refact] 매직 스트링 Enum으로 관리(#16)

--- src/main/java/com/onpurple/enums/ExpireEnum.java
+++ src/main/java/com/onpurple/enums/ExpireEnum.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 
 public enum ExpireEnum {
     //ACCESS : 30분, REFRESH : 7일
-    ACCESS_EXPIRE(1000 * 60 * 30),
+    ACCESS_EXPIRE(60000),
     REFRESH_EXPIRE(1000 * 60 * 60 * 24 * 7);
-    
-    
+
+
     private final long time;
 }
