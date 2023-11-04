45f162c1529c5abf88017bf92cdc9857a3b50205
lesu <46406965+leeesu@users.noreply.github.com>
2023-11-05 04:34:59 +0900

    [Refact] 메세지 WriteBack방식으로 변경, 스케줄러 설정  (#64)

--- src/main/java/com/onpurple/enums/RedisKeyEnum.java
+++ src/main/java/com/onpurple/enums/RedisKeyEnum.java
@@ -9,12 +9,23 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RedisKeyEnum {
+    /**
+     * Redis Key
+     */
 
+    // User 리프레시 토큰 관리 키
     REFRESH_TOKEN_KEY("RefreshToken:"),
 
+    // Chat 메세지 저장을 위한 ChatRoom 관리 키
+    ChatRoom_KEY("ChatRoom:"),
 
-    // Cache Key
+
+    /**
+     * Cache Key
+     */
+    // User 로그인 관리 키
     LOGIN_USER_KEY("User:"),
+    // User 리스트 관리 키
     USER_LIST_KEY("UserList:"),
     ;
 
